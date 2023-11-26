3c93080963842dd2083df7d38def4c12cbf7ef13
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2023-11-26 23:19:50 +0530

    reducer Component comments updated

--- src/reducers/FormReducer.jsx
+++ src/reducers/FormReducer.jsx
@@ -1,3 +1,4 @@
+// Defines the initial state for the user data form
 let initialState = {
   first_name: "",
   last_name: "",
@@ -17,26 +18,31 @@ let initialState = {
   userCreationTimeStamp: 0,
 };
 
+// Reducer function to handle state updates based on actions
 function reducer(state, action) {
   switch (action.type) {
     case "inputChange":
+      // Updates the corresponding form field with the provided value
       return {
         ...state,
         [action.payLoad.name]: action.payLoad.value,
       };
     case "selctState": {
+      // Updates the 'state name' field with the selected state
       return {
         ...state,
         [action.payLoad.name]: action.payLoad.value,
       };
     }
     case "gender": {
+      // Updates the 'gender' field with the selected gender
       return {
         ...state,
         gender: action.payLoad.value,
       };
     }
     case "creationDate": {
+      // Updates the user creation date, time, and timestamp
       return {
         ...state,
         userCreationDate: action.payLoad.userCreationDate,
@@ -45,6 +51,7 @@ function reducer(state, action) {
       };
     }
     case "dob": {
+      // Parses the date of birth string and updates the 'dob' field
       let dob = Date.parse(action.payLoad.value);
       let dt = new Date(dob).toISOString().slice(0, 10);
 
@@ -54,12 +61,14 @@ function reducer(state, action) {
       };
     }
     case "userType": {
+      // Updates the 'admin' field based on the selected user type
       return {
         ...state,
         admin: action.payLoad.value,
       };
     }
     case "userStatus": {
+      // Updates the 'userStatus' field based on the selected user status
       let res = {
         ...state,
         [action.payLoad.name]: action.payLoad.value,
@@ -68,9 +77,11 @@ function reducer(state, action) {
       return res;
     }
     case "init": {
+      // Resets the state to the initial state
       return initialState;
     }
     case "updateUser": {
+      // Updates the state with the provided user data
       let res = {
         ...state,
         ...action.payLoad,
@@ -79,9 +90,11 @@ function reducer(state, action) {
       return res;
     }
     default: {
+      // Return the initial state if no matching action is found
       return initialState;
     }
   }
 }
 
+// Exports the 'initialState' and 'reducer' functions
 export { initialState, reducer };
