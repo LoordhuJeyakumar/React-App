a36e40ec100229439fc64d0de2330c479622271b
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2023-11-26 23:09:06 +0530

    App Component comments updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,56 +15,76 @@ import DeleteUsersModal from "./components/DeleteUsersModal";
 const DataContext = createContext(null);
 
 function App() {
+  // Imports the 'useLocation' hook from React Router
   const location = useLocation();
-  const [homePageComponent, setHomePageComponent] = useState(<Dashboard />);
+
+  // Initializes the 'data' state with an empty array
   const [data, setData] = useState([]);
+  // Initializes the 'loadingData' state with a false value
   const [loadingData, setLoadingData] = useState(false);
+  // Initializes the 'isUserAdded' state with a false value
   const [isUserAdded, setIsUserAdded] = useState(false);
 
+  // useEffect hook to fetch user details from JSON Server
   useEffect(() => {
+    // Checks if data is not already loading
     if (!loadingData) {
-      axios.get("http://localhost:3000/usersDetails").then((response) => {
-        setData(response.data);
-        setLoadingData(true);
-      });
+      // Uses axios to make a GET request to the 'http://localhost:3000/usersDetails' endpoint
+      axios
+        .get("http://localhost:3000/usersDetails")
+        .then((response) => {
+          // Sets the 'data' state with the response data
+          setData(response.data);
+          // Sets the 'loadingData' state to true to indicate data is loaded
+          setLoadingData(true);
+        })
+        .catch((error) => {
+          //If api request fail Logs the error to the console
+          console.error(error);
+        });
     }
-  }, [isUserAdded]);
+  }, [isUserAdded]); //This dependency array ensures the useEffect hook is only called when the 'isUserAdded' state changes
 
   return (
     <div>
-      <NavComponent
-        homePageComponent={homePageComponent}
-        setHomePageComponent={setHomePageComponent}
-      />
+      {/* Render the NavComponent */}
+      <NavComponent />
+
+      {/* Check if data is loading */}
       {loadingData ? (
+        /* Wrap components in DataContextProvider */
         <DataContext.Provider
           value={{
-            data,
-            setData,
-            loadingData,
-            setLoadingData,
-            isUserAdded,
-            setIsUserAdded,
+            data, //Global data state
+            setData, //State Function to update data state
+            loadingData, // Flag indicating if data is loading
+            setLoadingData, //State Function to set loadingData flag
+            isUserAdded, // Flag indicating if user has been added
+            setIsUserAdded, //State Function to set isUserAdded flag
           }}
         >
+          {/* Define routes using Routes component */}
           <Routes location={location}>
+            {/* Route for Dashboard page, Create User page, All Users page, Edit Users page, Delete Users page */}
             <Route path="/" element={<Dashboard />} />
             <Route path="create-users" element={<CreateUser />} />
             <Route path="all-users" element={<AllUsers />}>
+              {/* Nested route for View User Modal */}
               <Route path=":userID" element={<ViewUserModal />} />
-              <Route path="admin" />
             </Route>
 
             <Route path="edit-users" element={<EditUsers />}>
+              {/* Nested route for Edit User Modal */}
               <Route path=":userID" element={<EditUserModal />} />
             </Route>
             <Route path="delete-users" element={<DeleteUsers />}>
+              {/* Nested route for Delete Users Modal */}
               <Route path=":userID" element={<DeleteUsersModal />} />
             </Route>
           </Routes>
-          {/* {homePageComponent} */}
         </DataContext.Provider>
       ) : (
+        // Display loading indicator while data is loading
         <div className="d-flex gap-3 align-items-center justify-content-center">
           <div className="spinner-border " role="status">
             <span className="sr-only">Loading...</span>
--- src/main.jsx
+++ src/main.jsx
@@ -10,34 +10,11 @@ import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { BrowserRouter } from "react-router-dom";
-import * as Popper from "@popperjs/core"
-
-const usersDetails = [
-  {
-    id: 1,
-    firstName: "Loordhu",
-    lastName: "Jeyakumar",
-    email: "loordhujeyakumar@gmail.com",
-    phone: 9600693684,
-    dob: "03-05-1994",
-    qualification: "+1",
-    city: "Tenkasi",
-    state: "Tamil Nadu",
-    address: "3/525-19, new csi church street, melameignanapuram",
-    gender: "male",
-    admin: true,
-    userStatus: true,
-    fullname: () => {
-      return `${this.firstName} ${this.lastName}`;
-    },
-  },
-];
-
+import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
