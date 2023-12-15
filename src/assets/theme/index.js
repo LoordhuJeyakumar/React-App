507ea54f412ba6d338bf7fc4dc2c6bb7ebbcfcac
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2023-12-15 12:55:23 +0530

    delete useless files

--- src/assets/theme/index.js
+++ src/assets/theme/index.js
@@ -33,48 +33,41 @@ import rgba from "../../assets/theme/functions/rgba";
 
 // Soft UI Dashboard React components base styles for @mui material components
 import sidenav from "../../assets/theme/components/sidenav";
-import list from "../../assets/theme/components/list";
+/* import list from "../../assets/theme/components/list";
 import listItem from "../../assets/theme/components/list/listItem";
-import listItemText from "../../assets/theme/components/list/listItemText";
+import listItemText from "../../assets/theme/components/list/listItemText"; */
 import card from "../../assets/theme/components/card";
 import cardMedia from "../../assets/theme/components/card/cardMedia";
 import cardContent from "../../assets/theme/components/card/cardContent";
 /* import button from "../../assets/theme/components/button"; */
-import iconButton from "../../assets/theme/components/iconButton";
+/* import iconButton from "../../assets/theme/components/iconButton"; */
 /* import inputBase from "../../assets/theme/components/form/inputBase"; */
-import menu from "../../assets/theme/components/menu";
-import menuItem from "../../assets/theme/components/menu/menuItem";
+/* import menu from "../../assets/theme/components/menu";
+import menuItem from "../../assets/theme/components/menu/menuItem"; */
 /* import switchButton from "../../assets/theme/components/form/switchButton"; */
 import divider from "../../assets/theme/components/divider";
-import tableContainer from "../../assets/theme/components/table/tableContainer";
+/* import tableContainer from "../../assets/theme/components/table/tableContainer";
 import tableHead from "../../assets/theme/components/table/tableHead";
-import tableCell from "../../assets/theme/components/table/tableCell";
-import linearProgress from "../../assets/theme/components/linearProgress";
-import breadcrumbs from "../../assets/theme/components/breadcrumbs";
-import slider from "../../assets/theme/components/slider";
+import tableCell from "../../assets/theme/components/table/tableCell"; */
+/* import linearProgress from "../../assets/theme/components/linearProgress"; */
+/* import breadcrumbs from "../../assets/theme/components/breadcrumbs"; */
+/* import slider from "../../assets/theme/components/slider"; */
 import avatar from "../../assets/theme/components/avatar";
-import tooltip from "../../assets/theme/components/tooltip";
-import appBar from "../../assets/theme/components/appBar";
-import tabs from "../../assets/theme/components/tabs";
-import tab from "../../assets/theme/components/tabs/tab";
-import stepper from "../../assets/theme/components/stepper";
+/* import tooltip from "../../assets/theme/components/tooltip"; */
+/* import appBar from "../../assets/theme/components/appBar"; */
+/* import tabs from "../../assets/theme/components/tabs";
+import tab from "../../assets/theme/components/tabs/tab"; */
+/* import stepper from "../../assets/theme/components/stepper";
 import step from "../../assets/theme/components/stepper/step";
 import stepConnector from "../../assets/theme/components/stepper/stepConnector";
 import stepLabel from "../../assets/theme/components/stepper/stepLabel";
-import stepIcon from "../../assets/theme/components/stepper/stepIcon";
-/* import select from "../../assets/theme/components/form/select";
-import formControlLabel from "../../assets/theme/components/form/formControlLabel";
-import formLabel from "../../assets/theme/components/form/formLabel";
-import checkbox from "../../assets/theme/components/form/checkbox";
-import radio from "../../assets/theme/components/form/radio";
-import autocomplete from "../../assets/theme/components/form/autocomplete";
-import input from "../../assets/theme/components/form/input"; */
-import container from "../../assets/theme/components/container";
-import popover from "../../assets/theme/components/popover";
-import buttonBase from "../../assets/theme/components/buttonBase";
-import icon from "../../assets/theme/components/icon";
-import svgIcon from "../../assets/theme/components/svgIcon";
-import link from "../../assets/theme/components/link";
+import stepIcon from "../../assets/theme/components/stepper/stepIcon"; */
+/* import container from "../../assets/theme/components/container"; */
+/* import popover from "../../assets/theme/components/popover"; */
+/* import buttonBase from "../../assets/theme/components/buttonBase"; */
+/* import icon from "../../assets/theme/components/icon"; */
+/* import svgIcon from "../../assets/theme/components/svgIcon"; */
+/* import link from "../../assets/theme/components/link"; */
 
 export default createTheme({
   breakpoints: { ...breakpoints },
@@ -94,39 +87,38 @@ export default createTheme({
     MuiCssBaseline: {
       styleOverrides: {
         ...globals,
-        ...container,
       },
     },
     MuiDrawer: { ...sidenav },
-    MuiList: { ...list },
+    /* MuiList: { ...list },
     MuiListItem: { ...listItem },
-    MuiListItemText: { ...listItemText },
+    MuiListItemText: { ...listItemText }, */
     MuiCard: { ...card },
     MuiCardMedia: { ...cardMedia },
     MuiCardContent: { ...cardContent },
-    /* MuiButton: { ...button }, */
-    MuiIconButton: { ...iconButton },
+/*     MuiButton: { ...button }, */
+/*     MuiIconButton: { ...iconButton }, */
 /*     MuiInputBase: { ...inputBase }, */
-    MuiMenu: { ...menu },
-    MuiMenuItem: { ...menuItem },
-    /* MuiSwitch: { ...switchButton }, */
+/*     MuiMenu: { ...menu },
+    MuiMenuItem: { ...menuItem }, */
+/*     MuiSwitch: { ...switchButton }, */
     MuiDivider: { ...divider },
-    MuiTableContainer: { ...tableContainer },
+    /*   MuiTableContainer: { ...tableContainer },
     MuiTableHead: { ...tableHead },
-    MuiTableCell: { ...tableCell },
-    MuiLinearProgress: { ...linearProgress },
-    MuiBreadcrumbs: { ...breadcrumbs },
-    MuiSlider: { ...slider },
+    MuiTableCell: { ...tableCell }, */
+    /*   MuiLinearProgress: { ...linearProgress }, */
+    /*     MuiBreadcrumbs: { ...breadcrumbs }, */
+    /*     MuiSlider: { ...slider }, */
     MuiAvatar: { ...avatar },
-    MuiTooltip: { ...tooltip },
-    MuiAppBar: { ...appBar },
-    MuiTabs: { ...tabs },
-    MuiTab: { ...tab },
-    MuiStepper: { ...stepper },
+    /*     MuiTooltip: { ...tooltip }, */
+    /*     MuiAppBar: { ...appBar }, */
+    /*     MuiTabs: { ...tabs },
+    MuiTab: { ...tab }, */
+    /*     MuiStepper: { ...stepper },
     MuiStep: { ...step },
     MuiStepConnector: { ...stepConnector },
     MuiStepLabel: { ...stepLabel },
-    MuiStepIcon: { ...stepIcon },
+    MuiStepIcon: { ...stepIcon }, */
     /* MuiSelect: { ...select },
     MuiFormControlLabel: { ...formControlLabel },
     MuiFormLabel: { ...formLabel },
@@ -136,10 +128,10 @@ export default createTheme({
     MuiInput: { ...input },
     MuiOutlinedInput: { ...input },
     MuiFilledInput: { ...input }, */
-    MuiPopover: { ...popover },
-    MuiButtonBase: { ...buttonBase },
-    MuiIcon: { ...icon },
-    MuiSvgIcon: { ...svgIcon },
-    MuiLink: { ...link },
+    /*     MuiPopover: { ...popover }, */
+    /*     MuiButtonBase: { ...buttonBase }, */
+    /*     MuiIcon: { ...icon }, */
+    /* MuiSvgIcon: { ...svgIcon }, */
+    /*     MuiLink: { ...link }, */
   },
 });
